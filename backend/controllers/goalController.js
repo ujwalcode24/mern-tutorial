d92dc96eb56ecfc2c36c7e27e37fb3abbc146086
ujwalchoudhary <ujwal@logicloop.io>
2022-05-29 20:28:22 +0530

    added schema and model

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,8 +1,10 @@
 const expressAsyncHandler = require('express-async-handler')
 const asynHandler = require('express-async-handler')
+const Goal = require('../models/goalModel')
 
 const getGoals = asynHandler(async(req, res) => {
-    res.status(200).json({ message: "get goals" })
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
 
 const addGoals = asynHandler( async(req, res)=>{
@@ -10,15 +12,30 @@ const addGoals = asynHandler( async(req, res)=>{
         res.status(400)
         throw new Error('Please add a text field')
     }
-    res.status(200).json({ message: "set goal" })
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+    res.status(200).json(goal)
 })
 
 const updateGoals = asynHandler(async(req, res)=>{
+    const goal = await Goal.findById(req.params.id)
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal not Found')
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
     res.status(200).json({ message: `update goal ${req.params.id}` })
 }
 )
 const deleteGoals =  asynHandler(async(req, res)=>{
-    res.status(200).json({ message: `delete goal ${req.params.id}` })
+    const goal = await Goal.findById(req.params.id)
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal not Found')
+    }
+    await Goal.deleteOne({id: req.params.id})
+    res.status(200).json(req.params.id)
 }
 )
 module.exports = { getGoals, addGoals, updateGoals, deleteGoals }
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const colors = require('colors')
 const goalRoutes = require('./routes/goalRoutes.js')
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 3000;
 
+connectDB()
 
 const app = express()
 
